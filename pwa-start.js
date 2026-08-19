5d9652686d3b282b85d6184f6ddb563d9b7914cb
ab4661158-create <ab4661158@gmail.com>
2026-08-19 23:08:13 +0530

    Fix live /shop Sell button at server response

--- pwa-start.js
+++ pwa-start.js
@@ -5,6 +5,44 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const response = require('express/lib/response');
+
+// Remove the Seller entry from the customer HTML at the server boundary.
+// This protects /shop even if an older customer.html or PWA cache is deployed.
+if (!response.__shriviCustomerSellRemoved) {
+  const originalSendFile = response.sendFile;
+
+  response.sendFile = function (filePath, options, callback) {
+    const isCustomerPage =
+      path.basename(String(filePath || '')).toLowerCase() === 'customer.html';
+
+    if (!isCustomerPage) {
+      return originalSendFile.call(this, filePath, options, callback);
+    }
+
+    const res = this;
+    const done = typeof options === 'function' ? options : callback;
+
+    fs.readFile(filePath, 'utf8', (error, html) => {
+      if (error) {
+        if (typeof done === 'function') return done(error);
+        return res.status(500).send('Customer page unavailable');
+      }
+
+      const cleaned = html
+        .replace(/<a\b[^>]*href\s*=\s*["']\/seller["'][^>]*>[\s\S]*?<\/a>/gi, '')
+        .replace(/<button\b[^>]*(?:\/seller|sell)[^>]*>[\s\S]*?<\/button>/gi, '')
+        .replace(/<a\b[^>]*>[\s\S]*?\bSell\b[\s\S]*?<\/a>/gi, '');
+
+      res.set('Cache-Control', 'no-store, no-cache, must-revalidate');
+      res.type('html').send(cleaned);
+
+      if (typeof done === 'function') done();
+    });
+  };
+
+  response.__shriviCustomerSellRemoved = true;
+}
 
 const application = express.application;
 const originalDescriptor = Object.getOwnPropertyDescriptor(application, 'listen');
